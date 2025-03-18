fa0d3ffb1ae06df2b97900d843cc1419769c168d
coenicorn <coenicorn.blauwzee@gmail.com>
2025-03-18 12:55:47 +0100

    add python control logic over serial

---
+++ plankmetwielen/control.py
@@ -0,0 +1,33 @@
+import time
+import serial
+import pyautogui
+
+screen_size_x, screen_size_y = pyautogui.size()
+
+ser = serial.Serial()
+
+ser.baudrate = 115200
+ser.port = "COM4"
+
+def sendSpeedFromMousePosition():
+    mouse_x, mouse_y = pyautogui.position()
+
+    speed_a = int((mouse_x / screen_size_x) * 254)
+    speed_b = int((mouse_y / screen_size_y) * 254)
+
+    ser.write(bytes([0, speed_a, 0, speed_b]))
+    ser.flush()
+
+def main():
+    ser.open()
+    ser.flush()
+
+    while True:
+        # sendSpeedFromMousePosition()
+        ser.write(bytes([0, 255, 0, 255]))
+        time.sleep(1)
+
+    ser.close()
+
+if __name__ == "__main__":
+    main()
