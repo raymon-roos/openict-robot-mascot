eb113188a4cff72e0212864b839a6760743fdc4d
coenicorn <coenicorn.blauwzee@gmail.com>
2025-04-04 15:38:21 +0200

    write python motorcontroller class

--- prototype/software/hardware_interface/motorcontrol.py
+++ prototype/software/hardware_interface/motorcontrol.py
@@ -1,66 +1,36 @@
-# '''
-
-# This is example code for controlling the esp32 connected to the motor driver
-
-# '''
-# import time
-# import serial
-# import pyautogui
-
-# screen_size_x, screen_size_y = pyautogui.size()
-
-# ser = serial.Serial()
-
-# ser.baudrate = 115200
-# ser.port = "COM4"
-
-# # The arguments for this function MUST be between (and including) 0-254, but may not be higher than 255
-# def sendMotorCommand(directionA: int, speedA: int, directionB: int, speedB: int):
-#     if 0 > directionA > 1 or 0 > speedA > 254 or 0 > directionB > 1 or 0 > speedB > 254:
-#         print("motor command is not in a valid range 0-254 for speeds or 0-1 for directions")
-#         return
-
-#     ser.flush()
-#     ser.write(bytes([directionA, speedA, directionB, speedB]))
-
-# def sendSpeedFromMousePosition():
-#     mouse_x, mouse_y = pyautogui.position()
-
-#     speed_a = int((mouse_x / screen_size_x) * 254)
-#     speed_b = int((mouse_y / screen_size_y) * 254)
-
-#     ser.write(bytes([0, speed_a, 0, speed_b]))
-#     ser.flush()
-
-# def main():
-#     ser.open()
-#     ser.flush()
-#     ser.write(bytes([0, 0, 0, 0]))
-#     time.sleep(5)
-
-#     # while True:
-#     #     # sendSpeedFromMousePosition()
-#     #     ser.write(bytes([0, 255, 0, 255]))
-#     #     time.sleep(1)
-#     for i in range(4):
-#         ser.write(bytes([0, 255, 0, 255]))
-#         time.sleep(1)
-#         ser.write(bytes([0, 0, 0, 255]))
-#         time.sleep(1)
-        
-
-#     ser.close()
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
 import serial
 
 class MotorController:
     serial = None
 
+    LEFT = 0
+    RIGHT = 1
+
     def __init__(self):
+        print("connecting to motor serial port")
+
         self.serial = serial.Serial('/dev/ttyUSB')
+
+    # on object destruction, close serial connection
+    def __del__(self):
+        print("closing serial connection")
+        self.serial.close()
+
+    def _isCorrectByteSize(self, val):
+        if val < 0:
+            raise ValueError(f"value {val} is out of range: less than 0")
+        elif val > 254:
+            raise ValueError(f"value {val} is out of range: higher than 254")
+        else:
+            return True
+
+    def _sendBytes(self, a, b, c, d):
+        if self._isCorrectByteSize(a) and self._isCorrectByteSize(b) and self._isCorrectByteSize(c) and self._isCorrectByteSize(d):
+            self.serial.write(bytes([a, b, c, d]))
+
+    def stop(self):
+        self._sendBytes(0, 0, 0, 0)
+
+    # drives both motors
+    def drive(self, direction1, speed1, direction2, speed2):
+        self._sendBytes(direction1, speed1, direction2, speed2)
