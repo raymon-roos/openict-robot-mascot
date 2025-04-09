fe0a9b91e912c68350a1360af072ddc618a88a81
coenicorn <coenicorn.blauwzee@gmail.com>
2025-04-09 11:20:14 +0200

    software patch

--- prototype/software/hardware_interface/ultrasonic.py
+++ prototype/software/hardware_interface/ultrasonic.py
@@ -3,13 +3,14 @@ import serial
 import time
 
 
-arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1) #'/dev/ttyACM0' ubuntu | 'COM4' windows
 
 class Rangefinder:
-      # def __init__(self):# Constructor (init-methode) wordt automatisch aangeroepen bij het maken van een object
+      def __init__(self, port='COM4'):
+            self.arduino = serial.Serial(port, baudrate=115200, timeout=.1) #'/dev/ttyACM0' ubuntu | 'COM4' windows
+
       def read(self):
-            arduino.write(bytes('0', 'utf-8'))  
-            data = arduino.readline().decode('utf-8') # utf-8' voor windows
+            self.arduino.write(bytes('0', 'utf-8'))  
+            data = self.arduino.readline().decode('utf-8') # utf-8' voor windows
             return data 
 
       def print_range(self): 
