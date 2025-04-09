5250ff4517eeb39b1700ef3fc95bd97b1ff41cfb
coenicorn <coenicorn.blauwzee@gmail.com>
2025-04-09 11:36:03 +0200

    ultrasonic code

--- prototype/software/main.py
+++ prototype/software/main.py
@@ -1,8 +1,11 @@
-# from hardware_interface.ultrasonic import Rangefinder
+from hardware_interface.ultrasonic import Rangefinder
 from hardware_interface.motorcontrol import MotorController
 import time
+import serial.tools.list_ports;
 
-def main():
+# https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+
+def drive_motor():
     motorcontroller = MotorController()
 
     motorcontroller.drive(MotorController.RIGHT, 100, MotorController.LEFT, 100)
@@ -13,7 +16,24 @@ def main():
     time.sleep(1)
     motorcontroller.stop()
 
+def read_ultrasonic():
+    reader = Rangefinder()
 
+    while True:
+        dst_str = reader.read()
+
+        if dst_str.__len__() == 0: continue
+
+        # print 3 floats
+        print()
+
+        time.sleep(0.1)
+
+def main():
+    print([comport.device for comport in serial.tools.list_ports.comports()])
+
+    # drive_motor
+    read_ultrasonic()
 
 if __name__ == "__main__":
     main()
