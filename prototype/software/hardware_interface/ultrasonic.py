46be55f702462d6610b6d207bce4a0bbfa42e748
coenicorn <coenicorn.blauwzee@gmail.com>
2025-04-09 11:35:42 +0200

    patch output list of floats instead of string

--- prototype/software/hardware_interface/ultrasonic.py
+++ prototype/software/hardware_interface/ultrasonic.py
@@ -7,11 +7,19 @@ import time
 class Rangefinder:
       def __init__(self, port='COM4'):
             self.arduino = serial.Serial(port, baudrate=115200, timeout=.1) #'/dev/ttyACM0' ubuntu | 'COM4' windows
+            self.arduino.read_all()
 
       def read(self):
-            self.arduino.write(bytes('0', 'utf-8'))  
-            data = self.arduino.readline().decode('utf-8') # utf-8' voor windows
-            return data 
+            # request data
+            self.arduino.write(b"0")
+            data = self.arduino.readline().decode('utf-8').split(",")
+
+            # return an array of 3 floating point values
+            if data.__len__() != 3:
+                  # check if data is received correctly
+                  return (float(0), float(0), float(0))
+            else:
+                  return (float(data[0]), float(data[1]), float(data[2]))
 
       def print_range(self): 
             value = self.read()
